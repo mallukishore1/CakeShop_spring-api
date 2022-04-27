de7eabe17acbae62ad3dd93ad0579308ed437545
kishore <you@example.comgit>
2022-04-27 17:36:16 +0530

    first commit

---
+++ src/main/java/com/cakeshop_springapi/CakeshopSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.cakeshop_springapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeshopSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeshopSpringApiApplication.class, args);
+	}
+
+}
